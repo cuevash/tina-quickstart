ca34954985623d17b1ec2b5eec70635f29ddca3a
Hector Cuevas <cuevash.sp@gmail.com>
2022-01-08 11:46:10 +0100

    Initial tina 101

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,35 @@
-import '../styles/index.css'
+import "../styles/index.css";
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import dynamic from "next/dynamic";
+import { TinaEditProvider } from "tinacms/dist/edit-state";
+const TinaCMS = dynamic(() => import("tinacms"), { ssr: false });
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      <TinaEditProvider
+        editMode={
+          <TinaCMS
+            clientId={process.env.NEXT_PUBLIC_TINA_CLIENT_ID}
+            branch={process.env.NEXT_PUBLIC_EDIT_BRANCH}
+            isLocalClient={Boolean(
+              Number(process.env.NEXT_PUBLIC_USE_LOCAL_CLIENT ?? true)
+            )}
+            cmsCallback={(cms) => {
+              import("react-tinacms-editor").then((field) => {
+                cms.plugins.add(field.MarkdownFieldPlugin);
+              });
+            }}
+            {...pageProps}
+          >
+            {(livePageProps) => <Component {...livePageProps} />}
+          </TinaCMS>
+        }
+      >
+        <Component {...pageProps} />
+      </TinaEditProvider>
+    </>
+  );
+};
+
+export default App;
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,23 +1,37 @@
-import { useRouter } from 'next/router'
-import ErrorPage from 'next/error'
-import Container from '../../components/container'
-import PostBody from '../../components/post-body'
-import Header from '../../components/header'
-import PostHeader from '../../components/post-header'
-import Layout from '../../components/layout'
-import { getPostBySlug, getAllPosts } from '../../lib/api'
-import PostTitle from '../../components/post-title'
-import Head from 'next/head'
-import { CMS_NAME } from '../../lib/constants'
-import markdownToHtml from '../../lib/markdownToHtml'
+import { useRouter } from "next/router";
+import ErrorPage from "next/error";
+import Container from "../../components/container";
+import PostBody from "../../components/post-body";
+import Header from "../../components/header";
+import PostHeader from "../../components/post-header";
+import Layout from "../../components/layout";
+import PostTitle from "../../components/post-title";
+import Head from "next/head";
+import { CMS_NAME } from "../../lib/constants";
+import { staticRequest } from "tinacms";
+import { useEffect, useState } from "react";
+import markdownToHtml from "../../lib/markdownToHtml";
 
-export default function Post({ post, morePosts, preview }) {
-  const router = useRouter()
-  if (!router.isFallback && !post?.slug) {
-    return <ErrorPage statusCode={404} />
+export default function Post({ data, slug }) {
+  const { title, coverImage, date, author, body, ogImage } =
+    data.getPostDocument.data;
+  const router = useRouter();
+
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    const parseMarkdown = async () => {
+      setContent(await markdownToHtml(body));
+    };
+
+    parseMarkdown();
+  }, [body]);
+
+  if (!router.isFallback && !slug) {
+    return <ErrorPage statusCode={404} />;
   }
   return (
-    <Layout preview={preview}>
+    <Layout preview={false}>
       <Container>
         <Header />
         {router.isFallback ? (
@@ -27,58 +41,84 @@ export default function Post({ post, morePosts, preview }) {
             <article className="mb-32">
               <Head>
                 <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
+                  {title} | Next.js Blog Example with {CMS_NAME}
                 </title>
-                <meta property="og:image" content={post.ogImage.url} />
+                <meta property="og:image" content={ogImage.url} />
               </Head>
               <PostHeader
-                title={post.title}
-                coverImage={post.coverImage}
-                date={post.date}
-                author={post.author}
+                title={title}
+                coverImage={coverImage}
+                date={date}
+                author={author}
               />
-              <PostBody content={post.content} />
+              <PostBody content={content} />
             </article>
           </>
         )}
       </Container>
     </Layout>
-  )
+  );
 }
 
-export async function getStaticProps({ params }) {
-  const post = getPostBySlug(params.slug, [
-    'title',
-    'date',
-    'slug',
-    'author',
-    'content',
-    'ogImage',
-    'coverImage',
-  ])
-  const content = await markdownToHtml(post.content || '')
+export const getStaticProps = async ({ params }) => {
+  const { slug } = params;
+  const variables = { relativePath: `${slug}.md` };
+  const query = `
+    query BlogPostQuery($relativePath: String!) {
+      getPostDocument(relativePath: $relativePath) {
+        data {
+          title
+          excerpt
+          date
+          coverImage
+          author {
+            name
+            picture
+          }
+          ogImage {
+            url
+          }
+          body
+        }
+      }
+    }
+  `;
+  const data = await staticRequest({
+    query: query,
+    variables: variables,
+  });
 
   return {
     props: {
-      post: {
-        ...post,
-        content,
-      },
+      query,
+      variables,
+      data,
+      slug,
     },
-  }
-}
+  };
+};
 
 export async function getStaticPaths() {
-  const posts = getAllPosts(['slug'])
-
-  return {
-    paths: posts.map((post) => {
-      return {
-        params: {
-          slug: post.slug,
-        },
+  const postsListData = await staticRequest({
+    query: `
+      query {
+        getPostList {
+          edges {
+            node {
+            sys {
+              filename
+              }
+            }
+          }
       }
-    }),
+    }
+    `,
+    variables: {},
+  });
+  return {
+    paths: postsListData.getPostList.edges.map((edge) => ({
+      params: { slug: edge.node.sys.filename },
+    })),
     fallback: false,
-  }
+  };
 }
